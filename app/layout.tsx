3ed548cabd0770b3fe2b21e757efdcc5029010c7
Priyansh Singh <singhpriyansh2014@gmail.com>
2026-07-05 17:50:01 +0530

    added home screen content with updating tailwind version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,32 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Space_Grotesk } from "next/font/google";
-import "./globals.css";
+import Navbar from '@/components/Navbar'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter, Space_Grotesk } from 'next/font/google'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+const inter = Inter({ subsets: ['latin'] })
 const spaceGrotesk = Space_Grotesk({ 
   subsets: ['latin'], 
   weight: ['300', '400', '500', '600', '700']
  })
 
 export const metadata: Metadata = {
-  title: "Shop Smart",
-  description: "Making shopping easier and smarter with AI",
-};
-
+  title: 'Pricewise',
+  description: 'Track product prices effortlessly and save money on your online shopping.',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="max-w-10xl mx-auto">
+          <Navbar />
+          {children}
+        </main>
+      </body>
     </html>
-  );
+  )
 }
