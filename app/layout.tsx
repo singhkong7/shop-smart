39f95c0920438e6f19317fafbc1693b37b636c37
Priyansh Singh <singhpriyansh2014@gmail.com>
2026-07-05 15:35:14 +0530

    added assets and global styling for app

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Space_Grotesk } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const spaceGrotesk = Space_Grotesk({ 
+  subsets: ['latin'], 
+  weight: ['300', '400', '500', '600', '700']
+ })
+
+export const metadata: Metadata = {
+  title: "Shop Smart",
+  description: "Making shopping easier and smarter with AI",
+};
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
