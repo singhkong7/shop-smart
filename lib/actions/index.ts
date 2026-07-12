5330211cf18d980be4fa6b5734ef2c1b4839a7d5
Priyansh Singh <singhpriyansh2014@gmail.com>
2026-07-12 10:43:56 +0530

    Added mongoDB Database

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,11 +1,13 @@
 "use server"
 
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 export async function scrapeAndStoreProduct(productUrl: string) {
  if(!productUrl) return;
  try {
+    connectToDB();
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
-    console.log('Scraped Product:>>>>>>>>>>>>>>>>>>', scrapedProduct);
+    // console.log('Scraped Product:>>>>>>>>>>>>>>>>>>', scrapedProduct);
  }
   catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`)
