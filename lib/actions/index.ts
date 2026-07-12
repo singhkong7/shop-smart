27ee63f89e452e54c77ecc76283e18d4c4ac0281
Priyansh Singh <singhpriyansh2014@gmail.com>
2026-07-12 11:05:30 +0530

    Updated Scrape and product function by storing the product in DB, and generating best price for the product from DB

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,15 +1,46 @@
-"use server"
-
+import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
+import {revalidatePath} from 'next/cache';
 import { scrapeAmazonProduct } from "../scraper";
+import { getLowestPrice,getHighestPrice,getAveragePrice } from "../utils";
+
 export async function scrapeAndStoreProduct(productUrl: string) {
- if(!productUrl) return;
- try {
+  if(!productUrl) return;
+
+  try {
     connectToDB();
+
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
-    // console.log('Scraped Product:>>>>>>>>>>>>>>>>>>', scrapedProduct);
- }
-  catch (error: any) {
+
+    if(!scrapedProduct) return;
+
+    let product = scrapedProduct;
+
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+
+    if(existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice }
+      ]
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      }
+    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+
+    revalidatePath(`/products/${newProduct._id}`);
+  } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`)
   }
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+import { PriceHistoryItem } from "@/types";
+
 // Extracts and returns the price from a list of possible elements.
 export function extractPrice(...elements: any) {
   for (const element of elements) {
@@ -24,4 +26,35 @@ export function extractPrice(...elements: any) {
 export function extractCurrency(element: any) {
   const currencyText = element.text().trim().slice(0, 1);
   return currencyText ? currencyText : "";
+}
+
+export function getHighestPrice(priceList: PriceHistoryItem[]) {
+  let highestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price > highestPrice.price) {
+      highestPrice = priceList[i];
+    }
+  }
+
+  return highestPrice.price;
+}
+
+export function getLowestPrice(priceList: PriceHistoryItem[]) {
+  let lowestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price < lowestPrice.price) {
+      lowestPrice = priceList[i];
+    }
+  }
+
+  return lowestPrice.price;
+}
+
+export function getAveragePrice(priceList: PriceHistoryItem[]) {
+  const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
+  const averagePrice = sumOfPrices / priceList.length || 0;
+
+  return averagePrice;
 }
